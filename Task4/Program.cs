d78645db4384700f7dee752fe111ced3e83152fe
romanukalexandr <romanukalexandr@gmail.com>
2023-02-11 12:03:10 +0300

    add task4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+double a = double.Parse (Console.ReadLine ());
+
+Console.WriteLine("Введите второе число");
+double b = double.Parse (Console.ReadLine ());
+
+Console.WriteLine("Введите третье число");
+double c = double.Parse (Console.ReadLine ());
+
+double max = a;
+
+if (b>max)
+{
+    max = b;
+}
+if (c>max)
+{
+    max = c;
+}
+
+Console.WriteLine("Максимальное из трех чисел = "+max);
