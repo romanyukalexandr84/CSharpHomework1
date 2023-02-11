3d7b9b5a31d0fe5df63a1a7450631ae6c47fa943
romanukalexandr <romanukalexandr@gmail.com>
2023-02-11 13:31:50 +0300

    add task8

---
+++ Task8/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Введите число N");
+double N = double.Parse (Console.ReadLine ());
+
+int count = 2;
+
+if (N>=2)
+{
+    while (count<=N)
+    {
+        Console.WriteLine (count);
+        count = count+2;
+    }
+}
+else
+{
+    if (N>0)
+    {
+        Console.WriteLine ("Четных чисел от 1 до N нет");
+    }
+    else
+    {
+        count = 0;
+        while (count>=N)
+        {
+            Console.WriteLine (count);
+            count = count-2;
+        }
+    }
+}
