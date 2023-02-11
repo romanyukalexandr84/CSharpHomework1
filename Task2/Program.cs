a52964ca799c086eb53c726af8cde555a1387bdf
romanukalexandr <romanukalexandr@gmail.com>
2023-02-11 11:48:06 +0300

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите первое число");
+double a = double.Parse (Console.ReadLine ());
+
+Console.WriteLine("Введите второе число");
+double b = double.Parse (Console.ReadLine ());
+
+if (a>b)
+{
+    Console.WriteLine("max = "+a);
+    Console.WriteLine("min = "+b);
+}
+else 
+{
+if (b>a)
+{
+    Console.WriteLine("max = "+b);
+    Console.WriteLine("min = "+a);
+}
+else
+Console.WriteLine("числа равны");
+}
