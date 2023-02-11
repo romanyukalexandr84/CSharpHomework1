6bc60e234c3473570c9a4f1b48f8d94a85f7f66b
romanukalexandr <romanukalexandr@gmail.com>
2023-02-11 12:20:20 +0300

    add task6

---
+++ Task6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число");
+double a = double.Parse (Console.ReadLine ());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine ("Число является четным");
+}
+else
+{
+    Console.WriteLine ("Число является нечетным");
+}
